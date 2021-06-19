7faf874f6bbc28e99a52c83c7ec00a2e53dcd6db
Sungwoo Park <codest99@gmail.com>
2021-06-19 17:11:20 +0900

    chore: DOM 태그들의 아이디 다는 로직 추가

---
+++ src/dom-utils.js
@@ -0,0 +1,7 @@
+export default function initDOM() {
+  const carGameContainerDivs = document.querySelectorAll('.car-game-container > div');
+  carGameContainerDivs[0].querySelector('input').id = 'car-names-input';
+  carGameContainerDivs[0].querySelector('button').id = 'car-names-submit';
+  carGameContainerDivs[1].querySelector('input').id = 'racing-count-input';
+  carGameContainerDivs[1].querySelector('button').id = 'racing-count-submit';
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import initDOM from './dom-utils.js';
+
 export default function RacingCarGame() {
-	
+  initDOM();
 }
 
 new RacingCarGame();
