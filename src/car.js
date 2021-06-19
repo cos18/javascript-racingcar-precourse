5ffe82d75d977fae02a491c46abf901add945c8e
Sungwoo Park <codest99@gmail.com>
2021-06-19 19:42:36 +0900

    chore: Car 객체 구현

--- src/car.js
+++ src/car.js
@@ -1,4 +1,12 @@
 export default function Car(name) {
   this.name = name;
   this.pos = 0;
+
+  this.go = () => {
+    if (Math.floor(Math.random() * 10) >= 4) {
+      this.pos += 1;
+    }
+  };
+
+  this.getStatus = () => `${this.name}: ${'-' * this.pos}`;
 }
